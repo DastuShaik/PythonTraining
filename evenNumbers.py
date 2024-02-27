a43a6fc555b1066b1200b1ac116bd3ac7bbeb22d
Dastagir Shaik <59111187+DastuShaik@users.noreply.github.com>
2024-02-26 19:21:12 -0600

    Add files via upload

---
+++ evenNumbers.py
@@ -0,0 +1,12 @@
+"""
+Python Program to count even numbers in a list assuming '0' as even 
+
+"""
+
+print('Enter the numbers in the list:')
+c=0
+#lst=list(map(int,input().split()))
+lst=[2,4,4,5,2,55,0,54,45,12]
+for i in range(len(lst)):
+	if lst[i]%2==0: c+=1
+print(c)
